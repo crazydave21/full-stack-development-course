1c38614f73eaf7e1f186154cba1cf4aaa5b3620d
CrazyDave <adrieldavid81169@gmail.com>
2023-03-01 22:02:15 +0530

    added optional syntax

--- javascript/stringMethods/includes.js
+++ javascript/stringMethods/includes.js
@@ -1,4 +1,6 @@
 let string = "How are you?";
+let string1 = "How you are";
 
 //returns true if string is present
 console.log(string.includes("you"));
+console.log(string1.includes("you", 5)); // false
