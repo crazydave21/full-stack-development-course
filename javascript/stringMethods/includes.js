f2640291fbc92c90b081c337738857a402199106
CrazyDave <adrieldavid81169@gmail.com>
2023-03-01 22:15:22 +0530

    added example for optional syntax

--- javascript/stringMethods/includes.js
+++ javascript/stringMethods/includes.js
@@ -3,4 +3,5 @@ let string1 = "How you are";
 
 //returns true if string is present
 console.log(string.includes("you"));
+//starts search at position 5
 console.log(string1.includes("you", 5)); // false
