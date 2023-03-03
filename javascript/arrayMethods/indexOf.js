a9f1760c3cac6c8793b9830f44f8e1eea22a7935
CrazyDave <adrieldavid81169@gmail.com>
2023-03-03 16:16:21 +0530

    updated comments

--- javascript/arrayMethods/indexOf.js
+++ javascript/arrayMethods/indexOf.js
@@ -4,4 +4,4 @@ let array = ["John", "Dave", "Jill", "Jack", "Jill"];
 //searches "John" and returns its index
 console.log(array.indexOf("John")); // 0
 //searches "Jill" after index 3 and returns its index
-console.log(array.indexOf("Jill", 3));
+console.log(array.indexOf("Jill", 3)); // 4
