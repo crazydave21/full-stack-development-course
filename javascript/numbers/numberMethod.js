5c1803196a808cb58b4187c513c7ae7270b909cc
CrazyDave <adrieldavid81169@gmail.com>
2023-03-10 16:51:40 +0530

    Initial

---
+++ javascript/numbers/numberMethod.js
@@ -0,0 +1,10 @@
+let a = "5";
+let b = true;
+
+//converts string to number
+let num = Number(a);
+//converts boolean to number
+let boolean = Number(b);
+
+console.log(num); //  5
+console.log(boolean); // 1
