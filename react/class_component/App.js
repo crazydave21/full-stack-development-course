488c504157382f4bbf56eb9f7a3a266585ad2e90
CrazyDave <adrieldavid81169@gmail.com>
2023-05-14 22:50:27 +0530

    Initial

---
+++ react/class_component/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';  
+class App extends Component{  
+   render(){  
+      return(  
+         <div>  
+            <h1>This is a Class Component</h1>
+            <p>Hello World</p>
+         </div>  
+      );  
+   }  
+}  
+export default App; 
