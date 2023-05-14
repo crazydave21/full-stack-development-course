f5193132bd218ad12c20dc7af7e3a672f73b94a7
CrazyDave <adrieldavid81169@gmail.com>
2023-05-14 23:06:51 +0530

    Initial

---
+++ react/functional_component/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';    
+function App() {
+    return(
+        <div>
+            <h1>This is a Functional Component</h1>
+            <p>Hello World</p>
+        </div>
+    )   
+}  
+export default App; 
