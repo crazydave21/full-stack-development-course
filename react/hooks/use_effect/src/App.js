cd532b3a466ad65225dacd6a711e1cf0325dec18
CrazyDave <adrieldavid81169@gmail.com>
2023-07-02 12:14:29 +0530

    Added example for optional syntax

--- react/hooks/use_effect/src/App.js
+++ react/hooks/use_effect/src/App.js
@@ -1,25 +1,74 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useEffect } from "react";
+// //without depedency
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [reset, setReset] = useState(false);
+//   useEffect(() => {
+//     if (!reset) {
+//       const timer = () => setCount(count + 1);
+//       setTimeout(timer, 1000);
+//     } else return;
+//   });
+//   return (
+//     <div className="App">
+//       <p>{"Count : " + count}</p>
+//       <button
+//         onClick={() => {
+//           setReset(true);
+//         }}
+//       >
+//         stop
+//       </button>
+//     </div>
+//   );
+// }
 
+// export default App;
+
+// //with empty depedency
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [reset, setReset] = useState(false);
+//   useEffect(() => {
+//     if (!reset) {
+//       const timer = () => setCount(count + 1);
+//       setTimeout(timer, 1000);
+//     } else return;
+//   },[]);
+//   return (
+//     <div className="App">
+//       <p>{"Count : " + count}</p>
+//       <button
+//         onClick={() => {
+//           setReset(true);
+//         }}
+//       >
+//         stop
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//with depedency
 function App() {
   const [count, setCount] = useState(0);
-  const [reset, setReset] = useState(false);
+  const [action, setAction] = useState(0);
   useEffect(() => {
-    if (!reset) {
-      const timer = () => setCount(count + 1);
-      setTimeout(timer, 1000);
-    } else return;
-  });
+    setCount(count + 1);
+  }, [action]);
   return (
     <div className="App">
       <p>{"Count : " + count}</p>
       <button
         onClick={() => {
-          setReset(true);
+          setAction(action + 1);
         }}
       >
-        stop
+        Run once
       </button>
     </div>
   );
